1c47ffe4fa8b1e10757cdc21caa056aec7aaa409
carolinaaceromartin-spec <carolina.acero.aristos@gmail.com>
2025-11-08 00:58:15 +0100

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,10 @@
+# Este es un script de Python sencillo que imprime el mensaje "Hola Mundo".
+
+def saludar():
+    """Imprime el saludo clásico en la consola."""
+    print("¡Hola Mundo! Este es mi primer script en GitHub.")
+
+# Llama a la función para ejecutar el saludo
+if __name__ == "__main__":
+    saludar()
+
